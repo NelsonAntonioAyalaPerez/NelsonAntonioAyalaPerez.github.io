63bd2a931f6fac5bdf37e5a8c583167340230a35
NelsonAntonioAyalaPerez <NelsonAntonioAyalaPerez@gmail.com>
2021-11-13 17:35:36 -0600

    Subida de archivos

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+const firebaseConfig = {
+    apiKey: "AIzaSyBAt2ROXhKywbt4vK-DO0ixEC1uQLOvs00",
+    authDomain: "fbautent.firebaseapp.com",
+    projectId: "fbautent",
+    storageBucket: "fbautent.appspot.com",
+    messagingSenderId: "711451952560",
+    appId: "1:711451952560:web:e61e158c4d6f91c98d8275",
+    measurementId: "G-H6WD1HLMSM"
+};
